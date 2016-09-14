96af2b825388c6b3c4719c359e1ddbf1201cfa4a
João Gabriel <Joo Gabriel>
2016-09-14 11:32:29 -0300

    second commit

---
+++ application/views/welcome_message.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<meta http-equiv="X-UA-Compatible" content="IE=edge">
+	<title>View Style</title>
+	<link rel="stylesheet" href="<?php echo base_url("js/jquery-ui.css");?>">
+	<link rel="stylesheet" href="<?php echo base_url("css/bootstrap.css");?>">
+	<link rel="stylesheet" href="<?php echo base_url("css/main.css");?>">
+</head>
+<body class="container">
+	<input class="form-control"></input>
+	<button class="btn btn-primary">primary</button>
+	<button class="btn btn-warning">warning</button>
+	<button class="btn btn-success">success</button>
+	<button class="btn btn-danger">danger</button>
+	<button class="btn btn-default">default</button>
+</body>
+</html>
+
+
